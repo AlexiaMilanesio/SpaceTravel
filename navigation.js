7d7e929457827fca4922fc4cd22e9a63fd00dba6
AlexiaMilanesio <alexiamilanesio9@gmail.com>
2023-01-25 13:01:52 +0100

    technology section

--- navigation.js
+++ navigation.js
@@ -1,14 +1,26 @@
 const nav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
-navToggle.addEventListener("click", () => {
-    
-    const visiblity = nav.getAttribute("data-visible");
-    if (visiblity === "false") {
+
+// when someone clicks the hamburger button
+navToggle.addEventListener("click", () =>
+{
+    const visibility = nav.getAttribute("data-visible");
+     // if the nav is closed, open it
+     if(visibility === "false")
+     {
         nav.setAttribute("data-visible", true);
-        navToggle.setAttribute("aria-expanded", true);
-    } else {
+        navToggle.setAttribute("aria-expanded", true)
+     }
+      // if the nav is open, close it
+     else
+     {
         nav.setAttribute("data-visible", false);
-        navToggle.setAttribute("aria-expanded", false);
-    }
+        navToggle.setAttribute("aria-expanded", false)
+     }
+    console.log(navToggle.getAttribute('aria-expanded'));
 })
+
+
+const lightFavicon = document.getElementById('light-favicon');
+const darkFavicon = document.getElementById('dark-favicon');
